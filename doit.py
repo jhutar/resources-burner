df9c6f6253000720b7ac24e64e1a6d8967e8ec78
Jan Hutar <jhutar@redhat.com>
2024-01-03 10:49:38 +0100

    feat: Add possibility to execute disk

--- doit.py
+++ doit.py
@@ -7,6 +7,7 @@ import psutil
 import logging
 import logging.handlers
 import time
+import os
 
 import pympler.asizeof
 
@@ -45,25 +46,55 @@ def setup_logger(stderr_log_lvl):
     return logging.getLogger("root")
 
 
-def child(loops, cpu_load, memory_load):
+def child(**kwargs):
     logger = logging.getLogger("child")
-    logger.debug(f"Started child witl loops={loops} cpu_load={cpu_load} memory_load={memory_load}")
+    logger.debug(f"Started child with {kwargs}")
 
     store = []
-    store_once = " " * memory_load
+    store_once = " " * kwargs["memory_load"]
+
+    if kwargs["disk_write_load"] > 0:
+        write_load = b" " * kwargs["disk_write_load"]
+        write_path = kwargs["disk_write_destination"].replace("{i}", str(kwargs["iteration"])).replace("{p}", str(kwargs["process"]))
+        write_fd = open(write_path, "wb", buffering=kwargs["disk_buffer"])
+
+    if kwargs["disk_read_load"] > 0:
+        read_load = bytearray(kwargs["disk_read_load"])
+        read_path = kwargs["disk_read_source"].replace("{i}", str(kwargs["iteration"])).replace("{p}", str(kwargs["process"]))
+        read_fd = open(read_path, "rb", buffering=kwargs["disk_buffer"])
+        assert os.path.getsize(kwargs["disk_read_source"]) > 0, "File to read have to have size > 0"
 
     loop = 0
-    while loop < loops:
+    while loop < kwargs["loops"]:
         # This simulates some actual CPU workload
         inner = 0
-        while inner < cpu_load:
+        while inner < kwargs["cpu_load"]:
             inner += 1
 
         # This simulates some actual consumed memory
         # store.append(store_once)   # no copy if reusing the same string!
-        store.append((str(loop) + store_once)[:memory_load])
+        store.append((str(loop) + store_once)[:kwargs["memory_load"]])
+
+        # This simulated some actual disk write activity
+        if kwargs["disk_write_load"] > 0:
+            write_fd.write(write_load)
+
+        # This simulated some actual disk read activity
+        if kwargs["disk_read_load"] > 0:
+            remaining = kwargs["disk_read_load"]
+            while remaining > 0:
+                read = read_fd.readinto(read_load)
+                remaining -= read
+                if read == 0:
+                    logger.debug("Skipping to the beginning of the read file")
+                    read_fd.seek(0)
 
         loop += 1
+
+    if kwargs["disk_write_load"] > 0:
+        write_fd.close()
+    if kwargs["disk_read_load"] > 0:
+        read_fd.close()
 
     self_process = multiprocessing.current_process()
     psutil_process = psutil.Process(self_process.pid)
@@ -74,15 +105,22 @@ def child(loops, cpu_load, memory_load):
     )
 
 
-def spawn(args):
+def spawn(args, iteration):
     logger = logging.getLogger("spawn")
     processes_list = list()
     kwargs = {
+        "iteration": iteration,
         "loops": args.loops,
         "cpu_load": args.cpu_load,
         "memory_load": args.memory_load,
+        "disk_write_load": args.disk_write_load,
+        "disk_write_destination": args.disk_write_destination,
+        "disk_read_load": args.disk_read_load,
+        "disk_read_source": args.disk_read_source,
+        "disk_buffer": args.disk_buffer,
     }
-    for _ in range(args.processes):
+    for i in range(args.processes):
+        kwargs.update({"process": i})
         p = multiprocessing.Process(target=child, kwargs=kwargs)
         p.start()
         processes_list.append(p)
@@ -137,6 +175,36 @@ def main():
         type=int,
     )
     parser.add_argument(
+        "--disk-write-load",
+        help="How many bytes to write to the disk per loop per process, use 0 to disable",
+        default=0,
+        type=int,
+    )
+    parser.add_argument(
+        "--disk-write-destination",
+        help="File name template to write to, use '{i}' for iteration and '{p}' for process",
+        default="/var/tmp/resources-burner-writes-{i}-{p}.data",
+        type=str,
+    )
+    parser.add_argument(
+        "--disk-read-load",
+        help="How many bytes to read from the disk per loop per process, use 0 to disable",
+        default=0,
+        type=int,
+    )
+    parser.add_argument(
+        "--disk-read-source",
+        help="File name template to to read from, use '{i}' for iteration and '{p}' for process, you can create file with: dd if=/dev/urandom of=... bs=... count=...",
+        default="/var/tmp/resources-burner-reads.data",
+        type=str,
+    )
+    parser.add_argument(
+        "--disk-buffer",
+        help="Open function buffer. -1 to use default, 0 to turn it off.",
+        default=-1,
+        type=int,
+    )
+    parser.add_argument(
         "-v",
         "--verbose",
         help="Verbose output",
@@ -168,12 +236,12 @@ def main():
         i = 0
         while True:
             logging.debug(f"Starting iteration {i}")
-            spawn(args=args)
+            spawn(args=args, iteration=i)
             i += 1
     else:
         for i in range(args.iterations):
             logging.debug(f"Starting iteration {i} of {args.iterations}")
-            spawn(args=args)
+            spawn(args=args, iteration=i)
 
 
 if __name__ == "__main__":
